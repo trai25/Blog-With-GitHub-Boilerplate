7767888e44f4d7f1ef8ad62a3b44707bcee57cd1
trai25 <trai25@users.noreply.github.com>
2019-12-25 01:43:17 +0800

    Update conf.py

--- conf.py
+++ conf.py
@@ -14,27 +14,27 @@ enable_jsdelivr = {
 }
 
 # 站点设置
-site_name = "我的个人博客"
+site_name = "F.Z.R."
 site_logo = "${static_prefix}logo.png"
 site_build_date = "2019-12-18T16:51+08:00"
-author = "熊猫小A"
-email = "hi@imalan.cn"
-author_homepage = "https://www.imalan.cn"
-description = "只坚持一种正义。我的正义。"
-key_words = ['Maverick', '熊猫小A', 'Galileo', 'blog']
+author = "trai25"
+email = "trai25@gmail.com"
+author_homepage = "https://www.trai25"
+description = "碎片 🧩"
+key_words = ['Maverick', 'Galileo', 'blog']
 language = 'zh-CN'
-external_links = [
-    {
-        "name": "Maverick",
-        "url": "https://github.com/AlanDecode/Maverick",
-        "brief": "🏄‍ Go My Own Way."
-    },
-    {
-        "name": "三無計劃",
-        "url": "https://www.imalan.cn",
-        "brief": "熊猫小A的主页。"
-    }
-]
+# external_links = [
+#     {
+#         "name": "Maverick",
+#        "url": "https://github.com/AlanDecode/Maverick",
+#         "brief": "🏄‍ Go My Own Way."
+#     },
+#     {
+#         "name": "三無計劃",
+#         "url": "https://www.imalan.cn",
+#         "brief": "熊猫小A的主页。"
+#    }
+# ]
 nav = [
     {
         "name": "首页",
@@ -56,20 +56,15 @@ nav = [
 social_links = [
     {
         "name": "Twitter",
-        "url": "https://twitter.com/AlanDecode",
+        "url": "https://twitter.com/trai25",
         "icon": "gi gi-twitter"
     },
-    {
-        "name": "GitHub",
-        "url": "https://github.com/AlanDecode",
-        "icon": "gi gi-github"
-    },
-    {
-        "name": "Weibo",
-        "url": "https://weibo.com/5245109677/",
-        "icon": "gi gi-weibo"
-    }
-]
+#     {
+#         "name": "GitHub",
+#         "url": "https://github.com/AlanDecode",
+#         "icon": "gi gi-github"
+#     }
+# ]
 
 head_addon = r'''
 <meta http-equiv="x-dns-prefetch-control" content="on">
